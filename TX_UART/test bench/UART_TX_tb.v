90e254bb4d8a07bc5db007e5d1a411454d06b7da
mohamedkhaled550 <mohamedkhaledscu@gmail.com>
2025-09-11 01:15:08 +0300

    Add files via upload

---
+++ TX_UART/test bench/UART_TX_tb.v
@@ -0,0 +1,39 @@
+module UART_TX_tb();
+reg clk, arst_n, tx_en;
+reg [7:0] data;
+wire tx, done, busy;
+UART_TX uut
+(
+	.clk(clk),
+	.arst_n(arst_n),
+	.tx_en(tx_en),
+	.data(data),
+	.tx(tx),
+	.done(done),
+	.busy(busy)
+);
+localparam T = 10;
+always
+begin
+	clk = 1'b0;
+#(T/2)
+	clk = 1'b1;
+#(T/2);
+end
+initial
+begin
+	arst_n = 1'b0;
+	tx_en = 1'b0;
+#2
+	arst_n = 1'b1;
+	tx_en = 1'b1;
+@(negedge clk)
+	data = 8'b01101010;
+wait(done == 1)
+#20 
+@(negedge clk)
+	data = 8'b01011011;
+wait(done == 1)
+#100 $stop;
+end
+endmodule
