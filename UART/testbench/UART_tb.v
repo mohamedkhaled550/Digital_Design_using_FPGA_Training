52cf13cd587bb6bc17608b2761c5e38156c010fc
mohamedkhaled550 <mohamedkhaledscu@gmail.com>
2025-09-12 21:35:13 +0300

    Update UART_tb.v

--- UART/testbench/UART_tb.v
+++ UART/testbench/UART_tb.v
@@ -1,5 +1,5 @@
 module UART_tb();
-reg clk, arst_n;
+reg clk, arst_n, rst;
 reg [7:0] data;
 reg uart_en;
 wire [7:0] received_data;
@@ -8,6 +8,7 @@ UART uut
 (
 	.clk(clk),
 	.arst_n(arst_n),
+	.rst(rst),
 	.data(data),
 	.uart_en(uart_en),
 	.received_data(received_data),
@@ -18,7 +19,7 @@ UART uut
 	.RX_error(RX_error)
 );
 initial
-	#50_000_000 $finish;
+	#5_000_000 $finish;
 localparam T = 10;
 always
 begin
@@ -31,8 +32,10 @@ initial
 begin
 	arst_n = 1'b0;
 	uart_en = 1'b0;
+	rst = 1;
 #2
 	arst_n = 1'b1;
+	rst = 0;
 	uart_en = 1'b1;
 	repeat(2)@(negedge clk)
 	data = 8'b11010011;
