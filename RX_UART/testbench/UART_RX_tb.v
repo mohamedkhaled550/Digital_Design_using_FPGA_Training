a69bf345b68c1ccd19440c840f1de773d1c9d70d
mohamedkhaled550 <mohamedkhaledscu@gmail.com>
2025-09-12 21:31:54 +0300

    Update UART_RX_tb.v

--- RX_UART/testbench/UART_RX_tb.v
+++ RX_UART/testbench/UART_RX_tb.v
@@ -1,10 +1,11 @@
 module UART_RX_tb();
-    reg clk, areset_n, RX, rx_en;
+    reg clk, areset_n, rst, RX, rx_en;
     wire [7:0] data;
     wire done, err, busy;
 	UART_RX uut (
         .clk(clk),
-        .areset_n(areset_n),
+        .arst_n(areset_n),
+		.rst(rst),
         .RX(RX),
         .rx_en(rx_en),
         .done(done),
@@ -13,13 +14,13 @@ module UART_RX_tb();
         .data(data)
     );
     // Clock generation (100 MHz)
-    localparam T = 10; // 10 ns ? 100 MHz
+    localparam T = 10;
     always #(T/2) clk = ~clk;
     // UART parameters
     localparam BAUD = 9600;
     localparam CLK_FREQ = 100_000_000;
-    localparam integer BIT_PERIOD = CLK_FREQ / BAUD;
-
+    localparam integer BIT_PERIOD = CLK_FREQ / BAUD; // ~10416 cycles
+    // Task to send one byte over RX
     task send_byte(input [7:0] byte);
         integer i;
         begin
@@ -38,16 +39,17 @@ module UART_RX_tb();
         clk = 0;
         RX = 1;        // idle
         areset_n = 0;
+		rst = 1;
         rx_en = 0;
         #(5*T);
         areset_n = 1;
+		rst = 0;
         rx_en = 1;
         #(5*T);
-
+	
         send_byte(8'b01011010);
-
+        // Wait a bit to observe "done"
         #(BIT_PERIOD*T*2);
         $stop;
 	end
 endmodule
-
